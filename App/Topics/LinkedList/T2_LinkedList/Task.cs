67ff1ba61d08af8ccd14bee01afcb09fbb00b771
user1 <user1@DESKTOP-ENRJJP4>
2025-10-18 15:05:42 +0300

    task 2

--- App/Topics/LinkedList/T2_LinkedList/Task.cs
+++ App/Topics/LinkedList/T2_LinkedList/Task.cs
@@ -1,1 +1,25 @@
 namespace App.Topics.LinkedList.T2_LinkedList;
+
+using System.Collections.Generic;
+
+public static class LinkedListTasks
+{
+    public static LinkedList<int> RemoveDuplicates(LinkedList<int> list)
+    {
+        if (list == null)
+            throw new ArgumentNullException(nameof(list));
+
+        LinkedList<int> result = new LinkedList<int>();
+        HashSet<int> uniqueInts = new HashSet<int>();
+
+        foreach (var item in list)
+        {
+            if (uniqueInts.Add(item))
+            {
+                result.AddLast(item);
+            }
+        }
+
+        return result;
+    }
+}
