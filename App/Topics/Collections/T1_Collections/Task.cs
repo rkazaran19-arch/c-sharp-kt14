dea3533b981ef889579bd7980920a66b5cd4ecba
user1 <user1@DESKTOP-ENRJJP4>
2025-10-18 14:36:56 +0300

    task 1

--- App/Topics/Collections/T1_Collections/Task.cs
+++ App/Topics/Collections/T1_Collections/Task.cs
@@ -1,1 +1,52 @@
+using System.Collections;
+
 namespace App.Topics.Collections.T1_Collections;
+
+public static class CollectionsTasks
+{
+    public static ArrayList FilterUniqueStringsNonGeneric(IEnumerable source)
+    {
+        ArrayList result = new ArrayList();
+        HashSet<string> uniqueStrings = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+        foreach (var item in source)
+        {
+            if (item is not string str)
+                continue;
+            if (string.IsNullOrWhiteSpace(str))
+                continue;
+
+            str = str.Trim();
+
+            if (uniqueStrings.Add(str))
+            {
+                result.Add(str); 
+            }
+        }
+
+        return result;
+    }
+
+    public static ArrayList FilterUniqueStringsGeneric(IEnumerable<string> source)
+    {
+        ArrayList result = new ArrayList();
+        HashSet<string> uniqueStrings = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+        foreach (var item in source)
+        {
+            if (string.IsNullOrWhiteSpace(item))
+                continue;
+
+           var str = item.Trim();
+
+            if (uniqueStrings.Add(str))
+            {
+                result.Add(str);
+            }
+        }
+
+        return result;
+    }
+}
+
+
